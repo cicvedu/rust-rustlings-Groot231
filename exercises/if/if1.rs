b002791fd89d8559b7f6f3a7d2865aba7212d1fc
Groot231 <139519550+Groot231@users.noreply.github.com>
2023-08-10 17:33:48 +0800

    Update if1.rs

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -9,10 +9,11 @@ pub fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    
     if a > b{
-        a;
+        a
     }else {
-        b;
+        b
     }
 }
 
